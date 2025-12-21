f2ba168859e87dd67e59b2eb4914b01f061bbe7e
KeyboardCombination <gamerbear256@gmail.com>
2025-12-20 23:35:20 -0500

    quaternion constructor variable names * also fix color.h lol

--- Client/App/util/Quaternion.cpp
+++ Client/App/util/Quaternion.cpp
@@ -4,48 +4,49 @@ namespace RBX
 {
 	Quaternion::Quaternion(const G3D::Matrix3& rot)
 	{
-		float v2 = rot[0][0] + rot[1][1] + rot[2][2];
-		if (v2 > 0)
+		// https://euclideanspace.com/maths/geometry/rotations/conversions/matrixToQuaternion/index.htm
+		float trace = rot[0][0] + rot[1][1] + rot[2][2];
+		if (trace > 0)
 		{
-			float v3 = sqrt(v2 + 1);
-			this->w = v3 * 0.5;
-			float v4 = 0.5 / v3;
-			this->x = (rot[2][1] - rot[1][2]) * v4;
-			this->y = (rot[0][2] - rot[2][0]) * v4;
-			this->z = (rot[1][0] - rot[0][1]) * v4;
+			float S = sqrt(trace + 1);
+			this->w = S * 0.5;
+			float reciprocal = 0.5 / S;
+			this->x = (rot[2][1] - rot[1][2]) * reciprocal;
+			this->y = (rot[0][2] - rot[2][0]) * reciprocal;
+			this->z = (rot[1][0] - rot[0][1]) * reciprocal;
 		}
 		else
 		{
 			if (rot[0][0] > rot[1][1] && rot[0][0] > rot[2][2])
 			{
-				float v5 = -sqrt(rot[0][0] + 1.0 - rot[1][1] - rot[2][2]);
-				this->x = v5 * 0.5;
-				float v6 = 0.5 / v5;
-				this->y = (rot[1][0] + rot[0][1]) * v6;
-				this->z = (rot[2][0] + rot[0][2]) * v6;
-				this->w = v6 * (rot[2][1] - rot[1][2]);
+				float S = -sqrt(rot[0][0] + 1.0 - rot[1][1] - rot[2][2]);
+				this->x = S * 0.5;
+				float reciprocal = 0.5 / S;
+				this->y = (rot[1][0] + rot[0][1]) * reciprocal;
+				this->z = (rot[2][0] + rot[0][2]) * reciprocal;
+				this->w = reciprocal * (rot[2][1] - rot[1][2]);
 			}
 			else
 			{
 				if (rot[1][1] > rot[2][2])
 				{
-					float v7 = -sqrt(rot[1][1] + 1.0 - rot[0][0] - rot[2][2]);
-					this->y = v7 * 0.5;
-					float v8 = 0.5 / v7;
-					this->x = (rot[1][0] + rot[0][1]) * v8;
-					this->z = (rot[2][1] + rot[1][2]) * v8;
+					float S = -sqrt(rot[1][1] + 1.0 - rot[0][0] - rot[2][2]);
+					this->y = S * 0.5;
+					float reciprocal = 0.5 / S;
+					this->x = (rot[1][0] + rot[0][1]) * reciprocal;
+					this->z = (rot[2][1] + rot[1][2]) * reciprocal;
 					float v9 = rot[0][2] - rot[2][0];
-					this->w = v8 * v9;
+					this->w = reciprocal * v9;
 				}
 				else
 				{
-					float v10 = -sqrt(rot[2][2] + 1.0 - rot[0][0] - rot[1][1]);
-					this->z = v10 * 0.5;
-					float v8 = 0.5 / v10;
-					this->x = (rot[2][0] + rot[0][2]) * v8;
-					this->y = (rot[2][1] + rot[1][2]) * v8;
+					float S = -sqrt(rot[2][2] + 1.0 - rot[0][0] - rot[1][1]);
+					this->z = S * 0.5;
+					float reciprocal = 0.5 / S;
+					this->x = (rot[2][0] + rot[0][2]) * reciprocal;
+					this->y = (rot[2][1] + rot[1][2]) * reciprocal;
 					float v9 = rot[1][0] - rot[0][1];
-					this->w = v8 * v9;
+					this->w = reciprocal * v9;
 				}
 			}
 		}
@@ -89,6 +90,7 @@ namespace RBX
 
 	void Quaternion::toRotationMatrix(G3D::Matrix3& rot) const
 	{
+		// https://euclideanspace.com/maths/geometry/rotations/conversions/quaternionToMatrix/index.htm
 		Quaternion QuatMul = *this * 2.0f;
 
 		float xx = x * QuatMul.x;
